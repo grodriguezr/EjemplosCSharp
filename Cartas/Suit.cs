898a57cb611957dec67f618e33d64f5763749629
grodriguezr <phoenix2303@hotmail.com>
2017-04-21 22:35:38 -0500

    Aplicación de cartas

---
+++ Cartas/Suit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cartas
+{
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade
+    }
+}
