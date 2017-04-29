d56920530f8caa57c8a6ce791d5e42b3ddf503ec
grodriguezr <phoenix2303@hotmail.com>
2017-04-29 15:47:56 -0500

    Colecciones fuertemente tipadas en la librería de cartas

---
+++ CartasCollection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartasCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Card tempCard = myDeck.GetCard(i);
+                Console.Write(tempCard.ToString());
+                if (i != 51) Console.Write(", ");
+                else Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
